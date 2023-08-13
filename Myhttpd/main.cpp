16ea4e88affcdded07143798151744ede945c64f
lsmin <lusimin1999@qq.com>
2023-08-14 01:12:46 +0800

    update

--- Myhttpd/main.cpp
+++ Myhttpd/main.cpp
@@ -1,7 +1,7 @@
 /*****************************************************************************
 * @文件名 main.cpp
 * @作者 : pkloi
-* @日期 : 2023/8/11 21:42
+* @日期 : 2023-8-13 16:38:04
 * @输入 : 
 * @输出 : 
 * @上一次修改 : 
@@ -16,6 +16,9 @@
 //网络通信需要包含的头文件和需要加载的库文件
 #include <WinSock2.h>
 #pragma comment(lib,"WS2_32.lib")
+
+//__FUNCTION__:当前函数名
+#define PRINTF(str) printf("[%s - %d]"#str" = %s\n",__FUNCTION__,__LINE__,str);
 
 //错误信息提示
 void error_die(const char* str)
@@ -83,25 +86,185 @@ int startUp(unsigned short *nPort)
 
 #pragma endregion
 
-#pragma region 设置套接字属性
-
-#pragma endregion
-
-#pragma region 网络通信初始化
-
+#pragma region 设置套接字属性，端口可复用性
+	//SOL_SOCKET:设置套接字的属性使用
+	//SO_REUSEADDR:使端口可以重复使用
+	int nOpt = 1;//是否可重复使用的值，1为可重复使用
+	nRet = setsockopt(nServer_sock, SOL_SOCKET, SO_REUSEADDR, (const char*)&nOpt, sizeof(nOpt));
+	if (nRet == -1)
+	{
+		error_die("端口可复用");
+	}
 #pragma endregion
 
 #pragma region 绑定套接字和网络地址
+	//配置服务器端的网络地址
+	struct sockaddr_in server_addr;
+	memset(&server_addr, 0, sizeof(server_addr));//清空设为0
+	server_addr.sin_family = PF_INET;//网络地址类型 PF_INET=>internetwork: UDP, TCP, etc.
+	server_addr.sin_port = htons(*nPort);//直接取端口可能有问题，需要转一下  htons(u_short)：本机转网络端 
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);//IP地址 INADDR_ANY：允许任何人访问
+
+	//绑定套接字
+	nRet = bind(nServer_sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
+	if (nRet < 0)
+	{
+		error_die("sockt bind");
+	}
 
 #pragma endregion
-	return -1;
+
+#pragma region 动态分配一个端口
+	int nNameLen = sizeof(server_addr);
+	if (*nPort == 0)//为0时才重新分配
+	{
+		/*getsockname:返回参数sockfd指定的本地IP和端口，
+		*				当套接字的地址与INADDR_ANY绑定时,除非使用connect或accept，否则函数将不返回本地IP的任何信息，
+		*				但是端口号可以返回
+		*/
+		nRet = getsockname(nServer_sock, (struct sockaddr*)&server_addr, &nNameLen);
+		if (nRet < 0)
+		{
+			error_die("getsockname");
+		}
+
+		*nPort = server_addr.sin_port;//getsockname获取到的ip和地址都会存入server_addr中
+	}
+#pragma  endregion
+
+#pragma region 创建监听地址
+	nRet = listen(nServer_sock, 5);//设置长度为5的监听队列
+	if (nRet < 0)
+	{
+		error_die("listen");
+	}
+#pragma endregion
+	return nServer_sock;
+}
+
+//从指定的客户端套接字，读取一行数据，保存到buff
+//返回实际读取到的字节数
+int get_line(int nSocket, char* buff, int nSize)
+{
+	char c = 0;//'\0'
+	int i = 0;
+
+	while (i < nSize - 1 && c != '\n')
+	{
+		int n = recv(nSocket, &c, 1, 0);
+		if (n > 0)
+		{
+			// \r\n  \r是回车，\n是换行
+			if (c == '\r')
+			{
+				//通常recv()函数的最后一个参数为0,代表从缓冲区取走数据,
+				//而当为MSG_PEEK时代表只是查看数据,而不取走数据
+				n = recv(nSocket, &c, 1, MSG_PEEK);
+				if (n > 0 && c == '\n')//正常情况下是\r\n，但不保证意外，需要判一下
+				{
+					recv(nSocket, &c, 1, 0);//是'\n'正常继续读
+				}
+				else
+				{
+					c = '\n';
+				}
+			}
+
+			buff[i++] = c;
+		}
+		else
+		{
+			c = '\n';
+		}
+	}
+
+	buff[i] = 0;//'\n'
+	return 0;
+}
+
+//向指定的套接字，发送一个提示还没有实现的错误页面
+void unimplement(int nClient)
+{
+	//to do
+}
+
+//处理用户请求的线程函数
+DWORD WINAPI accept_request(LPVOID arg)//LPVOID == void*  arg=>套接字
+{
+	//解析
+	char cBuff[1024];//1K
+
+	int nClient = (SOCKET)arg;//客户端套接字
+
+	//读一行数据
+	int nNumberChars = get_line(nClient, cBuff, sizeof(cBuff));
+	PRINTF(cBuff);
+
+	//获取提交方法 GET POST
+	char cMethod[255];
+	int i = 0, j = 0;
+	while (!isspace(cBuff[j]) && i < sizeof(cMethod) - 1)
+	{
+		cMethod[i++] = cBuff[j++];
+	}
+	cMethod[i] = 0;//'\n'
+	PRINTF(cMethod);
+
+	//检查提交的方法服务器是否支持
+	if (_stricmp(cMethod, "GET") && _stricmp(cMethod, "POST"))//_stricmp 比较时不区分大小写
+	{
+		//向浏览器返回一个错误页面
+		//to do 
+		unimplement(nClient);
+	}
+
+	//解析资源文件的路径
+	//"GET /test/abc.html HTTP/1.1\n"
+	char cURL[255];//存放请求的资源的完整路径
+	i = 0;
+	//跳过空格
+	while (isspace(cBuff[j]) && j < sizeof(cBuff))
+	{
+		j++;
+	}
+
+	while (!isspace(cBuff[j]) && i < sizeof(cURL) - 1 && j < sizeof(cBuff))
+	{
+		cURL[i++] = cBuff[j++];
+	}
+	cURL[i] = 0;
+	PRINTF(cURL);
+
+	return 0;
 }
 
 int main(void)
 {
-	unsigned short nPort = 80;
+	unsigned short nPort = 8000;
 	int nServer_sock = startUp(&nPort);
 
 	printf("httpd服务已经启动，正在监听 %d 端口...\n", nPort);
 
+	//让外部允许访问
+	struct sockaddr_in client_addr;//客户端
+	int nClientLen = sizeof(client_addr);
+
+	while (1)
+	{
+		//阻塞式等待用户通过浏览器发起访问
+		//有访问后，生成一个新的套接字
+		int nClient_sock = accept(nServer_sock, (struct sockaddr*)&client_addr, &nClientLen);//获取客户端的信息
+		if (nClient_sock == -1)
+		{
+			error_die("accept");
+		}
+
+		//创建线程
+		DWORD dwThreadID = 0;//线程唯一ID
+		//accept_request函数的参数为第三个参数nClient_sock传的值
+		CreateThread(0, 0, accept_request, (void*)nClient_sock, 0, &dwThreadID);
+	}
+
+	closesocket(nServer_sock);
+	return 0;
 }
